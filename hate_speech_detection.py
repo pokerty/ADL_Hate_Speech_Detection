865d5018a39f1055fc511ad2f833272a461d1a5d
pokerty <89121422+pokerty@users.noreply.github.com>
2023-05-14 20:01:48 +0200

    preprocessing incomplete, training works

--- hate_speech_detection.py
+++
@@ -1,7 +0,0 @@
-#imports
-from preproccessing import Preprocess
-
-if __name__ == "__main__":
-    preproccessed_data = Preprocess('\HASOC\english_dataset.tsv','\t')
-    #saving the preprocessed data
-    preproccessed_data.save()
